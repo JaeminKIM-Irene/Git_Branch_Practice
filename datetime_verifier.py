7518465d6238e1e3111df7874d95c894e8441e63
김재민 <jmkim@gimjaemin-ui-MacBookAir.local>
2023-12-26 18:44:17 +0900

    :sparkles: Feat: Complete class' internal functions
    Complete isHoliday and isAfternoon functions in DateTimeVerifier class

--- datetime_verifier.py
+++ datetime_verifier.py
@@ -10,12 +10,27 @@ class DateTimeVerifier() :
     def isHoliday(self) -> None :
         """
         """
-        pass
+
+        year = self.date.year
+        month = self.date.month
+        day = self.date.day
+        for holiday in self.holidays :
+            if holiday == self.date.strftime('%Y-%m-%d') :
+                print(f"입력하신 날짜 {year}년 {month}월 {day}일은 휴일입니다")
+                return
+        print(f"입력하신 날짜 {year}년 {month}월 {day}일은 휴일이 아닙니다")
+        return
 
     def isAfternoon(self) -> None:
         """
         """
-        pass
+
+        hour = self.time.hour
+        minute = self.time.minute
+        if hour < 12 :
+            print(f"현재 시간은 오전 {hour}시 {minute}분입니다")
+        else :
+            print(f"현재 시간은 오후 {hour-12}시 {minute}분입니다")
 
 if __name__ == '__main__' :
     now = datetime.now()
