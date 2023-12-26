ee0b3a4b8de498c2d514e42ed45e75bb59543591
김재민 <jmkim@gimjaemin-ui-MacBookAir.local>
2023-12-26 18:12:05 +0900

    :sparkles: Feat: Build outline of the verifier

---
+++ datetime_verifier.py
@@ -0,0 +1,17 @@
+class DateTimeVerifier() :
+
+    def __init__(self) :
+        pass
+
+    def isHoliday(self) :
+        """
+        """
+        pass
+
+    def isAfternoon(self) :
+        """
+        """
+        pass
+
+if __name__ == '__main__' :
+    pass
